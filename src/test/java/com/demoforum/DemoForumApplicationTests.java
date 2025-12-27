87da169c24f39daf5069892ff5549b6451a32f0b
yuk1 <23020110@vnu.edu.vn>
2025-12-28 05:30:16 +0700

    first

---
+++ src/test/java/com/demoforum/DemoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
